afb8ee8f8b3ec546aab9ca5dc97a2e80520c288c
amzadhossainjacky <amzadhossainjacky@gmail.com>
2026-05-05 18:49:26 +0600

    repo created

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Jacky.ai</h1>
+    </div>
+  );
+}
